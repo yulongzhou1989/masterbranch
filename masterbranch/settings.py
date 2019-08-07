9fec6afa6b5ff2f1fe85667e17d78750a6725995
yz <yz@yzdeMacBook-Pro.local>
2019-08-07 10:53:10 -0400

    remove template admin

--- masterbranch/settings.py
+++ masterbranch/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['masterbranch1.herokuapp.com', 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
