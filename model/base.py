139cc894c44c69dc7d253f71836c66b96b15aa9f
yz <yz@yzdeMacBook-Pro.local>
2019-08-28 12:38:17 -0400

    upload model base

---
+++ model/base.py
@@ -0,0 +1,9 @@
+from pynamodb.models import Model
+import json
+
+class BaseModel(Model):
+    def to_dict(self):
+        rval = {}
+        for key in self.attribute_values:
+            rval[key] = self.__getattribute__(key)
+        return rval
